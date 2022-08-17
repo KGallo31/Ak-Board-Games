3dd407f427b364a059be906f914c84133cd3b754
Kevin Gallo <Gallok731@gmail.com>
2022-08-17 14:21:10 -0400

    React Router setup

---
+++ client/src/components/App.js
@@ -0,0 +1,36 @@
+import React,{useState,useEffect} from 'react';
+import  {Route,Routes} from "react-router-dom"
+import Header from './Header';
+import '../App.css';
+
+
+
+function App() {
+
+  const [reviewState, setReviewState] = useState(true)
+
+
+  const changeReviewState = () => {
+    // if(selectedGame !== ''){
+    //   setReviewState(false)
+    // }
+    // else{
+    //   setReviewState(true)
+    // }
+  }
+
+
+
+  return (
+    <div className="App">
+      <div className="header">
+        <Header changeReviewState={changeReviewState}/>
+      </div>
+      {/* <Routes>
+        <Route path='/header' element={<Header changeReviewState={changeReviewState}/>}/>
+      </Routes> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Space = styled.p`
+  margin-top:40px;
+  margin-bottom:20px
+`;
+
+function Header({changeReviewState}) {
+  return (
+    <div className="center">
+      <Link to={`/`}>
+        <img 
+          onClick={changeReviewState} 
+          src="https://res.cloudinary.com/april-skrine/image/upload/v1645034174/newlogo_1_wjqh1k.jpg" 
+          alt="logo"
+        />
+      </Link>
+      <Space></Space>
+      <div>
+        <Link to={`/add-review`}><img src="https://res.cloudinary.com/april-skrine/image/upload/v1645035924/buttonback__1_-removebg-preview_ad3zbu.png" alt="add-review"/></Link> 
+      </div> 
+    </div>
+  )
+}
+export default Header
